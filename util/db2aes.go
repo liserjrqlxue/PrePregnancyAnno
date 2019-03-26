790b7b9a5a0d7360fa643547bdcdb41a584423cb
Wang Yaoshen <wangyaoshen@bgi.com>
2019-03-26 12:53:32 +0800

    convert db from excel to json

---
+++ util/db2aes.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"github.com/liserjrqlxue/simple-util"
+	"os"
+	"strings"
+)
+
+var (
+	prefix = flag.String(
+		"prefix",
+		"",
+		"output prefix.[all.lite].json, default is same to -excel")
+	excel = flag.String(
+		"excel",
+		"",
+		"PrePregnancy excel",
+	)
+	sheet = flag.String(
+		"sheet",
+		"database",
+		"PrePregnancy excel sheet name for database",
+	)
+	keys = flag.String(
+		"keys",
+		"Transcript:cHGVS",
+		"keys joint as map key",
+	)
+	sep = flag.String(
+		"sep",
+		":",
+		"sep of keys",
+	)
+)
+
+var headerMap = map[string]string{
+	"变异ID":                 "MutationID",
+	"ClinVar Significance": "ClinVar Significance",
+	"dbscSNV_ADA_SCORE":    "dbscSNV_ADA_SCORE",
+	"dbscSNV_RF_SCORE":     "dbscSNV_RF_SCORE",
+	"GERP++_RS":            "GERP++_RS",
+	"GWASdb_or":            "GWASdb_or",
+	"SIFT Pred":            "SIFT Pred",
+	"Polyphen2 HDIV Pred":  "Polyphen2 HDIV Pred",
+	"Polyphen2 HVAR Pred":  "Polyphen2 HVAR Pred",
+	"MutationTaster Pred":  "MutationTaster Pred",
+	"PP_disGroup":          "PP_disGroup",
+	"中文-疾病名称":              "Chinese desease name",
+	"遗传模式":                 "Inheritance",
+	"中文-突变详情":              "Chinese mutation information",
+	"中文-疾病简介":              "Chinese desease introduction",
+	"英文-疾病名称":              "English desease name",
+	"英文-突变详情":              "English mutation information",
+	"英文-疾病简介":              "English desease introduction",
+	"Evidence New + Check": "Evidence New + Check",
+	"Auto ACMG + Check":    "Auto ACMG + Check",
+	"Reference-final":      "Reference-final",
+	"Reference-final-Info": "Reference-final-Info",
+	"Database":             "Database",
+}
+
+func main() {
+	flag.Parse()
+	if *excel == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *prefix == "" {
+		*prefix = *excel
+	}
+	key := strings.Split(*keys, *sep)
+	_, db := simple_util.Sheet2MapArray(*excel, *sheet)
+	var allDb = make(map[string]map[string]string)
+	var liteDb = make(map[string]map[string]string)
+
+	for _, item := range db {
+		var keyValues []string
+		for _, k := range key {
+			keyValues = append(keyValues, item[k])
+		}
+		mainKey := strings.Join(keyValues, *sep)
+		allDb[mainKey] = item
+		var lite = make(map[string]string)
+		for k, v := range headerMap {
+			lite[v] = item[k]
+		}
+		liteDb[mainKey] = lite
+	}
+
+	alll, err := simple_util.JsonIndent(liteDb, "", "\t")
+	simple_util.CheckErr(err)
+	err = simple_util.Json2file(alll, *prefix+".lite.json")
+	simple_util.CheckErr(err)
+
+	allb, err := simple_util.JsonIndent(allDb, "", "\t")
+	simple_util.CheckErr(err)
+	err = simple_util.Json2file(allb, *prefix+".all.json")
+	simple_util.CheckErr(err)
+
+}
